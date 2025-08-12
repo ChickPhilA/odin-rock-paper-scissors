cb85c74a39a45c660610fba93b086387cf5615e3
Phil <natsudragoneel282003@gmail.com>
2025-08-12 12:30:34 -0700

    created a game simulation

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let choice = prompt("Enter your choice: 'rock', 'paper', or 'scissors'?")
     if(choice == 'rock' || choice == 'paper' || choice == 'scissors') {
+        choice = choice.toLowerCase() // makes the input case insensitive, accepting any form of input 
         return choice
     }
     else {
@@ -25,3 +26,52 @@ function getHumanChoice() {
         getHumanChoice()
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    // we start comparing the values here
+
+    if(humanChoice == "rock") {
+        if(computerChoice == "scissors") {
+            humanScore++
+            return "You win! Rock crushes scissors."
+        }
+        else if(computerChoice == "paper") {
+            computerScore++
+            return "You lose! Paper covers rock."
+        }
+        else{
+            return "It's a tie! Both chose rock."
+        }
+    }
+    else if(humanChoice == "paper") {
+        if(computerChoice == "scissors") {
+            computerScore++
+            return "You lose! Scissors cuts paper."
+        }
+        else if(computerChoice == "paper") {
+            return "It's a tie! Both chose paper."
+        }
+        else {
+            humanScore++
+            return "You win! Paper covers rock."
+        }
+    }
+    else {
+        if(computerChoice == "rock") {
+            computerScore++
+            return "You lose! Rock crushes scissors."
+        }
+        else if(computerChoice == "paper") {
+            humanScore++
+            return "You win! Scissors cuts paper."
+        }
+        else {
+            return "It's a tie! Both chose scissors."
+        }
+    }
+}
+
+let humanChoice = getHumanChoice()
+let computerChoice = getComputerChoice()
+
+playRound()
