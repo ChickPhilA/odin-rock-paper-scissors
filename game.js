24932cf7194ac193a6151a5aaac71b4a0debdb67
Phil <natsudragoneel282003@gmail.com>
2025-09-25 16:22:34 -0700

    added a determineWinner function

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-alert("You will play Rock, Paper, Scissors against the computer. First to 5 points wins!")
-
 /* DOM Instance Variables and Event Listeners */
 
 const rock = document.getElementById('rock')
@@ -32,8 +30,29 @@ function getComputerChoice() {
     }
 }
 
+function determineWinner(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        console.log("It's a tie!")
+        return "Tie"
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper") {
+                console.log("You win this round!")
+                humanScore++
+                playerScore.textContent = "Player Score: " + humanScore
+                return "Player"
+            }
+    else {
+        console.log("The computer wins this round!")
+        compScore++
+        computerScore.textContent = "Computer Score: " + compScore
+        return "Computer"
+    }
+}
+
 // This function will sequence a single round but will progress the whole game itself
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+function playRound(humanChoice, computerChoice) {
     let youChose = document.createElement('div')
     youChose.textContent = `You chose: `
 
@@ -55,67 +74,36 @@ function playRound(humanChoice, computerChoice, humanScore, computerScore) {
 
     document.querySelector('#you-chose').appendChild(youChose)
 
-    let cpuIsChoosing = document.createElement('div')
-    let cpuResponse = ""
-    cpuIsChoosing.textContent = "The computer is choosing..."
-    cpuIsChoosing.style.color = "red"
+    let cpu = document.createElement('div')
+    let cpuChoice = document.getElementById('computer-chose')
 
-    let timeout = setTimeout(() => {
-        document.querySelector('#computer-chose').appendChild(cpuIsChoosing)
-    }, 2000)
+    if(computerChoice === "rock") {
+        console.log("The CPU chose rock")
+        cpuResponse = "🪨"
+    }
+    
+    else if(computerChoice === "paper") {
+        console.log("The CPU chose paper")
+        cpuResponse = "📄"
+    }
+    else {
+        console.log("The CPU chose scissors")
+        cpuResponse = "✂️"
+    }
 
-    // This is when the CPU makes a choice
-    timeout = setTimeout(() => {
-        console.log("CPU generating a response...")
-        cpuResponse = getComputerChoice()
-    }, 2000)
+    cpu.textContent = "The CPU has chosen " + cpuResponse
+    cpuChoice.appendChild(cpu)
 
+    let result = determineWinner()
+    let resultText = document.createElement('div')
+    resultText.textContent = `Round ${round} Result: `
 
+    if(result == "player") {
 
-
-
+    }
 }
 
-// function playGame() {
-//     let humanScore = 0
-//     let computerScore = 0
 
-//     // initialize the choices as null first
-//     let humanChoice = null
-//     let computerChoice = null
-//     let result = null
-
-//     for(let i = 0; i < 5; i++) {
-//         humanChoice = getHumanChoice()
-//         computerChoice = getComputerChoice()
-//         result = playRound(humanChoice, computerChoice, humanScore, computerScore) // passing the score variables to increment them in other functions
-//         if(result == "win") {
-//             alert("You won the round!")
-//             humanScore++
-//         }
-//         else if(result == "lose") {
-//             alert("You lost the round!")
-//             computerScore++
-//         }
-//         else {
-//             alert("Round resulted in a tie.")
-//             // do nothing, it's a tie
-//         }
-//     }
-
-//     // after the 5 games have been played
-//     if(humanScore > computerScore) {
-//         alert(`Congratulations! You win the game with a score of ${humanScore} to ${computerScore}.`)
-//     }
-//     else if(computerScore > humanScore) {
-//         alert(`You lose the game! The computer wins with a score of ${humanScore} to ${humanScorecomputerScore}.`)
-//     }
-//     else {
-//         alert(`It's a tie! Both you and the computer scored ${humanScore}.`)
-//     }
-// }  
-
-
-rock.addEventListener('click', () => playRound("rock", getComputerChoice(), humanScore, compScore))
-paper.addEventListener('click', () => playRound("paper", getComputerChoice(), humanScore, compScore))
-scissors.addEventListener('click', () => playRound("scissors", getComputerChoice(), humanScore, compScore))
+rock.addEventListener('click', () => playRound("rock", getComputerChoice()))
+paper.addEventListener('click', () => playRound("paper", getComputerChoice()))
+scissors.addEventListener('click', () => playRound("scissors", getComputerChoice()))
